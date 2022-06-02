6a1f6ea58a4eec3f622bb07f8fb0e1fd258710a9
rkatzfus <100627135+rkatzfus@users.noreply.github.com>
2022-06-02 19:31:07 +0200

    Add files via upload

---
+++ class/mysqli.php
@@ -0,0 +1,42 @@
+<?php
+class mysqli_tools
+{
+    function __construct()
+    {
+        $this->username = get_current_user();
+        $host = 'db';
+        $user = 'MYSQL_USER';
+        $pass = 'MYSQL_PASSWORD';
+        $this->mysqli_conn = new mysqli($host, $user, $pass);
+        if ($this->mysqli_conn->connect_error) {
+            echo("Connection failed: " . $this->mysqli_conn->connect_error);
+            $this->mysqli_conn = false;
+            die();
+        } 
+    }
+    function __destruct() {
+        $this->mysqli_conn -> close();
+    }
+    public function sql2array( // exec_sql
+        $sql = ""
+    ) {
+        if ($this->mysqli_conn != false) {
+            foreach ($this->mysqli_conn -> query($sql) -> fetch_all(MYSQLI_ASSOC) as $value) {
+                $result[] = $value;
+            }
+            return $result;
+        }
+    }
+    public function sql2array_pk( // exec_sql
+        $sql = ""
+        , $pk = ""
+    ) {
+        if ($this->mysqli_conn != false) {
+            foreach ($this->mysqli_conn -> query($sql) -> fetch_all(MYSQLI_ASSOC) as $value) {
+                $result[$value[$pk]] = $value;
+            }
+            return $result;
+        }
+    }
+}
+?>
