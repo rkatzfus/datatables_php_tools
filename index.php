02a1a23def08517827c9e4f1e0e64b4085641c71
rkatzfus <100627135+rkatzfus@users.noreply.github.com>
2022-06-02 20:11:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use AutoloadingExample\Example;
+use AutoloadingExample\Plugin\ExamplePlugin;
+
+$example = new Example();
+$examplePlugin = new ExamplePlugin();
+?>
+
