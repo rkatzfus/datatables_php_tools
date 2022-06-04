a05535b017e5d5f2f1656a20d4dc7214893483de
RobCat <robert.katzfuss@googlemail.com>
2022-06-04 17:53:06 +0200

    test mysqli class

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use AutoloadingExample\Example;
-use AutoloadingExample\Plugin\ExamplePlugin;
-
 $example = new Example();
-$examplePlugin = new ExamplePlugin();
 ?>
 
