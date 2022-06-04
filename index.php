b3ca745fd23370c1e959372d1475ed968b2e4865
RobCat <robert.katzfuss@googlemail.com>
2022-06-04 21:56:20 +0200

    install mysqli ext

--- index.php
+++ index.php
@@ -3,6 +3,20 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Autoloading\database_tools;
 
-$objMySqli = new database_tools();
+$obj_mysqli = new database_tools();
+$sql = "
+    select
+        ID
+        , DEL
+        , NAME
+    from
+        MYSQL_DATABASE.test_table;
+";
+echo "<b>sql2array</b>";
+var_dump($obj_mysqli->sql2array($sql));
+echo "<hr>";
+echo "<b>sql2array_pk</b>";
+var_dump($obj_mysqli->sql2array_pk($sql,"NAME"));
+echo "<hr>";
 ?>
 
