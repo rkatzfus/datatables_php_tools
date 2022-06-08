98d00451f58729c751e55a1f2ef8ca0000f6f051
RobCat <robert.katzfuss@googlemail.com>
2022-06-08 21:50:36 +0200

    dt bs5 integration

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,30 +25,11 @@ class ComposerAutoloaderInit3a17cdf7e3c3e7edc81326acc03e1f52
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit3a17cdf7e3c3e7edc81326acc03e1f52', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3a17cdf7e3c3e7edc81326acc03e1f52', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit3a17cdf7e3c3e7edc81326acc03e1f52::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit3a17cdf7e3c3e7edc81326acc03e1f52::getInitializer($loader));
 
         $loader->register(true);
 
