1086ad7613796fa7c3c0c0b4749f805df287efe0
RobCat <robert.katzfuss@googlemail.com>
2022-06-20 22:24:20 +0200

    dropdown mulit 0.1

--- index_webutility.php
+++ index_webutility.php
@@ -27,7 +27,7 @@
 				$array_AJAX_xxxTESTxxx = array();
 				$array_AJAX_xxxTESTxxx["fetch"] = array(
 					"url" => "/vendor/datatableswebutility/dwuty/read.php"
-					, "datasource" => "MYSQL_DATABASE.root_table root"
+					, "datasource" => "MYSQL_DATABASE.root_table root left join MYSQL_DATABASE.ref_root_ref_dropdown_multi_table ref on ref.REF_ROOT = root.ID"
 				);
 				$obj_webutility = new webutility("dte_xxxTESTxxx", $array_AJAX_xxxTESTxxx, $pkfield_xxxTESTxxx);
 				$strsqlWhere_xxxTESTxxx = "root.DEL <> 1";
@@ -46,6 +46,25 @@
 						, "from" => "MYSQL_DATABASE.dropdown_lookup_table dropdown"
 					)
 				);
+				$arySetting_REF_DROPDOWN_MULTI = array(
+					"AJAX" => "/vendor/datatableswebutility/dwuty/read_select2.php"
+					, "SELECT2" => array(
+						"columns" => array(
+						  "id" => "ref.REF_ROOT"
+						  , "text" => "ref.REF_DROPDOWN_MULTI"
+						)
+						, "from" => "MYSQL_DATABASE.ref_root_ref_dropdown_multi_table ref"
+						, 'where' => 'ref.DEL<>1'
+					)
+					, "SUBSELECT2" => array(
+						"columns" => array(
+						  "id" => "dropdown_multi.ID"
+						  , "text" => "dropdown_multi.TEXT"
+						)
+						, "from" => "MYSQL_DATABASE.dropdown_multi_lookup_table dropdown_multi"
+						, 'where' => 'dropdown_multi.DEL<>1'
+					  )
+				);
 				$obj_webutility->new_column("root.TEXT", "TEXT", "column: TEXT", VIEW, TEXT);
 				$obj_webutility->new_column("root.EMAIL", "EMAIL", "column: EMAIL", VIEW, EMAIL);
 				$obj_webutility->new_column("root.CHECKBOX", "CHECKBOX", "column: CHECKBOX", VIEW, CHECKBOX, $arySetting_CHECKBOX);
@@ -53,6 +72,7 @@
 				$obj_webutility->new_column("root.LINK_BUTTON", "LINK_BUTTON", "column: LINK_BUTTON", VIEW, LINK_BUTTON);
 				$obj_webutility->new_column("root.COLOR", "COLOR", "column: COLOR", VIEW, COLOR);
 				$obj_webutility->new_column("root.REF_DROPDOWN", "DROPDOWN", "column: DROPDOWN", VIEW, DROPDOWN, $arySetting_DROPDOWN);
+				$obj_webutility->new_column("root.REF_DROPDOWN_MULTI", "DROPDOWN_MULTI", "column: DROPDOWN_MULTI", VIEW, DROPDOWN_MULTI, $arySetting_REF_DROPDOWN_MULTI);
 				$obj_webutility->new_column("root.DATE", "DATE", "column: DATE", VIEW, DATE);
 				$obj_webutility->new_column("root.DATETIME", "DATETIME", "column: DATETIME", VIEW, DATETIME);
 				$defOrderby_xxxTESTxxx = 0;
--- src/webutility_ssp.php
+++ src/webutility_ssp.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App;
+
 class webutility_ssp
 {
     private $draw;
@@ -11,256 +13,262 @@ class webutility_ssp
     private $data;
 
     public function __construct(
-        $debug=false
+        $debug = false
     ) {
         $this->debug = $debug;
         $this->draw = 0;
         $this->intlength = 10;
         $this->intstart = 0;
         $this->strsqlOrder = "";
+        $this->strGroupBy = "";
         $this->data = array();
         $this->obj_mysqli = new database_tools();
-    }  
+    }
     public function set_draw(
         $draw = 0
     ) {
         $this->draw = intval($draw);
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_draw</b><br>";
             echo $this->draw = intval($draw);
-        } 
+        }
     }
     public function set_length(
         $length = 10
     ) {
         $this->intlength = intval($length);
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_length</b><br>";
             echo $this->intlength = intval($length);
-        }  
+        }
     }
     public function set_start(
         $intstart = 0
     ) {
         $this->intstart = intval($intstart);
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_start</b><br>";
             echo $this->intstart = intval($intstart);
-        }  
+        }
     }
     private function length_and_paging()
     {
-        if($this->intlength==-1){
+        if ($this->intlength == -1) {
             return "";
-        }else{
-            return " limit ".$this->intstart.", ".$this->intlength;
+        } else {
+            return " limit " . $this->intstart . ", " . $this->intlength;
         }
     }
     public function set_order(
-        $orders = array()
-        , $columns = array()
+        $orders = array(),
+        $columns = array()
     ) {
         $sql_order = array();
         foreach ($orders as $ordersValue) {
-            array_push($sql_order, $columns[$ordersValue['column']]['data'].' '.$ordersValue['dir']);
+            array_push($sql_order, $columns[$ordersValue["column"]]["data"] . " " . $ordersValue["dir"]);
         }
-        $this->strsqlOrder = " order by ".implode(", ", $sql_order);
-        if ($this->debug == true ) {
+        $this->strsqlOrder = " order by " . implode(", ", $sql_order);
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_order</b><br>";
-            echo $this->strsqlOrder = " order by ".implode(", ", $sql_order);
-        } 
+            echo $this->strsqlOrder = " order by " . implode(", ", $sql_order);
+        }
     }
     private function set_recordsTotal()
     {
-        if(!isset($this->recordsTotal) && $this->recordsTotal < 1){
-            $sql = "
-                select
-                    distinct count(*) ".$this->strSqlFrom;
+        if (!isset($this->recordsTotal) && $this->recordsTotal < 1) {
+            $sql = "select count(*) " . $this->strSqlFrom . $this->strGroupBy;
             $this->recordsTotal = intval($this->obj_mysqli->sql_getfield($sql));
         }
     }
     private function set_recordsFiltered()
     {
-        if(!isset($this->recordsFiltered) && $this->recordsFiltered < 1){
+        if (!isset($this->recordsFiltered) && $this->recordsFiltered < 1) {
             $strWhereblock = "";
             if (!empty($this->strSqlWhere)) {
-                $strWhereblock .= "(".$this->strSqlWhere.")";
+                $strWhereblock .= "(" . $this->strSqlWhere . ")";
             }
             if (!empty($this->strSqlSearch)) {
                 if (empty($strWhereblock)) {
-                    $strWhereblock .= "(".$this->strSqlSearch.")";
+                    $strWhereblock .= "(" . $this->strSqlSearch . ")";
                 } else {
-                    $strWhereblock .= " and (".$this->strSqlSearch.")";
-                }  
+                    $strWhereblock .= " and (" . $this->strSqlSearch . ")";
+                }
             }
             if (!empty($this->strSqlSearchColumn)) {
                 if (empty($strWhereblock)) {
-                    $strWhereblock .= "(".$this->strSqlSearchColumn.")";
+                    $strWhereblock .= "(" . $this->strSqlSearchColumn . ")";
                 } else {
-                    $strWhereblock .= " and (".$this->strSqlSearchColumn.")";
-                }  
+                    $strWhereblock .= " and (" . $this->strSqlSearchColumn . ")";
+                }
             }
             if (!empty($strWhereblock)) {
-                $strWhereblock = " WHERE ".$strWhereblock;
+                $strWhereblock = " WHERE " . $strWhereblock;
             }
-            $sql = "
-                select
-                    distinct count(*) ".$this->strSqlFrom.$strWhereblock;
+            $sql = "select count(*) " . $this->strSqlFrom . $strWhereblock . $this->strGroupBy;
             $this->recordsFiltered = intval($this->obj_mysqli->sql_getfield($sql));
         }
     }
     public function set_Select(
         $ary_Select = array()
-    ) { 
-        $this->strsqlSelectStart = "select distinct ";
+    ) {
+        $this->strsqlSelectStart = "select ";
         $this->ary_sqlSelectInline = array();
         if (array_search('DT_RowId', array_column($ary_Select, 'dt')) !== false) {
             foreach ($ary_Select as $Select_value) {
                 if ($Select_value['dt'] == 'DT_RowId') {
                     $this->strsqlSelectStart .= "concat('row_', " . $Select_value['db'] . ") as DT_RowId";
                 } else {
-                    $this->ary_sqlSelectInline[] = $Select_value['db']." as ".$Select_value['dt'];
+                    $this->ary_sqlSelectInline[] = $Select_value['db'] . " as " . $Select_value['dt'];
                 }
             }
             $this->strsqlSelect = $this->strsqlSelectStart . ", " . implode(",", $this->ary_sqlSelectInline);
         } elseif (!empty($ary_Select)) {
             foreach ($ary_Select as $Select_key => $Select_value) {
-                $this->ary_sqlSelectInline[] = $Select_value . " as " . $Select_key ;
+                $this->ary_sqlSelectInline[] = $Select_value . " as " . $Select_key;
             }
             $this->strsqlSelect =  $this->strsqlSelectStart . implode(",", $this->ary_sqlSelectInline);
-        } else { 
-           $this->debug = true;
-           echo "<hr>";
-           echo "<b>an error has occured!</b>";
+        } else {
+            $this->debug = true;
+            echo "<hr>";
+            echo "<b>an error has occured!</b>";
         }
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_Select</b><br>";
             echo $this->strsqlSelect;
             var_dump($ary_Select);
-        }   
+        }
     }
     public function set_From(
-        $strSqlFrom
+        $strSqlFrom = ""
     ) {
-        $this->strSqlFrom = " from ".$strSqlFrom;
-        if ($this->debug == true ) {
+        $this->strSqlFrom = " from " . $strSqlFrom;
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_From</b><br>";
-            echo $this->strSqlFrom = " from ".$strSqlFrom;;
-        }   
+            echo $this->strSqlFrom = " from " . $strSqlFrom;
+        }
+    }
+    public function set_GroupBy(
+        $aryGroupBy = array()
+    ) {
+        $this->strGroupBy = " group by  " . implode(",", $aryGroupBy);
+        if ($this->debug == true) {
+            echo "<hr>";
+            echo "<b>function set_GroupBy</b><br>";
+            echo $this->strGroupBy = " group by  " . implode(",", $aryGroupBy);
+        }
     }
     public function set_Where(
-        $strSqlWhere=""
+        $strSqlWhere = ""
     ) {
         $this->strSqlWhere = $strSqlWhere;
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_Where</b><br>";
             echo $this->strSqlWhere = $strSqlWhere;
-        }   
+        }
     }
     public function set_Search(
         $strSqlSearch
     ) {
         $this->strSqlSearch = $strSqlSearch;
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_Search</b><br>";
             echo $this->strSqlSearch = $strSqlSearch;
-        } 
+        }
     }
     public function set_SearchColumn(
         $strSqlSearchColumn
     ) {
         $this->strSqlSearchColumn = $strSqlSearchColumn;
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_SearchColumn</b><br>";
             echo $this->strSqlSearchColumn = $strSqlSearchColumn;
-        } 
+        }
     }
     public function set_Columns(
-        $arycolumns
+        $arycolumns = array()
     ) {
         $this->arycolumns = $arycolumns;
         foreach ($arycolumns as $column) {
-            $this->arycolumns_id[$column['dt']] = $column['db'];
+            $this->arycolumns_id[$column["dt"]] = $column["db"];
         }
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_Columns</b><br>";
             var_dump($this->arycolumns_id);
-        } 
+        }
     }
-    public function set_data_sql() 
+    public function set_data_sql()
     {
         $strWhereblock = "";
         if (!empty($this->strSqlWhere)) {
-            $strWhereblock .= "(".$this->strSqlWhere.")";
+            $strWhereblock .= "(" . $this->strSqlWhere . ")";
         }
         if (!empty($this->strSqlSearch)) {
             if (empty($strWhereblock)) {
-                $strWhereblock .= "(".$this->strSqlSearch.")";
+                $strWhereblock .= "(" . $this->strSqlSearch . ")";
             } else {
-                $strWhereblock .= " and (".$this->strSqlSearch.")";
-            }  
+                $strWhereblock .= " and (" . $this->strSqlSearch . ")";
+            }
         }
         if (!empty($this->strSqlSearchColumn)) {
             if (empty($strWhereblock)) {
-                $strWhereblock .= "(".$this->strSqlSearchColumn.")";
+                $strWhereblock .= "(" . $this->strSqlSearchColumn . ")";
             } else {
-                $strWhereblock .= " and (".$this->strSqlSearchColumn.")";
-            }  
+                $strWhereblock .= " and (" . $this->strSqlSearchColumn . ")";
+            }
         }
         if (!empty($strWhereblock)) {
-            $strWhereblock = " where ".$strWhereblock;
+            $strWhereblock = " where " . $strWhereblock;
         }
-        $result = $this->obj_mysqli->sql2array($this->strsqlSelect.$this->strSqlFrom.$strWhereblock.$this->strsqlOrder.$this->length_and_paging());
-        if($result != false && isset($this->arycolumns)){
+        $sql = $this->strsqlSelect . $this->strSqlFrom . $strWhereblock . $this->strGroupBy . $this->strsqlOrder . $this->length_and_paging();
+        $result = $this->obj_mysqli->sql2array($sql);
+        if ($result != false && isset($this->arycolumns)) {
             foreach ($result as $value_query) {
                 $rowdata = array();
                 foreach ($this->arycolumns as $column) {
-                    $rowdata[$column['dt']] = $value_query[$column['dt']];
+                    $rowdata[$column["dt"]] = $value_query[$column["dt"]];
                 }
-                array_push($this->data,$rowdata);
+                array_push($this->data, $rowdata);
             }
         }
-        $sql = $this->strsqlSelect.$this->strSqlFrom.$strWhereblock.$this->strsqlOrder.$this->length_and_paging();
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_data_sql (final sql)</b><br>";
             echo $sql;
-        } 
+        }
         return  $sql;
     }
     public function set_data(
-        $data
+        $data = ""
     ) {
         $this->data = ($data);
-        if ($this->debug == true ) {
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function set_data</b><br>";
             echo $this->data = ($data);
-        } 
+        }
     }
     public function fetch()
     {
-        $result['draw'] = $this->draw;
+        $result["draw"] = $this->draw;
         $this->set_recordsTotal();
-        $result['recordsTotal'] = $this->recordsTotal;
+        $result["recordsTotal"] = $this->recordsTotal;
         $this->set_recordsFiltered();
-        $result['recordsFiltered'] = $this->recordsFiltered;
-        $result['data'] = $this->data;
-        if ($this->debug == true ) {
+        $result["recordsFiltered"] = $this->recordsFiltered;
+        $result["data"] = $this->data;
+        if ($this->debug == true) {
             echo "<hr>";
             echo "<b>function fetch</b><br>";
-        } 
+        }
         echo json_encode($result);
     }
 }
-?>
